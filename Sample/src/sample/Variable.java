c31dcdf6fe909c3ea1877ba39295a07ebaa7b4aa
dir-FY24katodaichi <daichi.kato.b566@dirbato.co.jp>
2024-04-08 12:49:16 +0900

    最初のコミット

---
+++ Sample/src/sample/Variable.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        var secondsPerDay = 86400;
+
+        System.out.println(secondsPerDay);  // printlnで出力してデータを確認
+    }
+}
